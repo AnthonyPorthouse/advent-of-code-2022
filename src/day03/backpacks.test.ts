03cea523839d0d751b09185e903eced60432fe95
Anthony Porthouse <anthony@porthou.se>
2022-12-03 11:28:36 +0000

    test: add tests around split backpack generation

--- src/day03/backpacks.test.ts
+++ src/day03/backpacks.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, it } from "vitest";
 import {
+  getBackpacksFromFile,
   getFullBackpacksFromFile,
   getItemInAllBackpacks,
-  getItemInBothBackpacks,
+  getItemInBothBackpacks
 } from "./backpacks.js";
+import exp from "constants";
 
 const exampleFile = new URL(`./example.txt`, import.meta.url).pathname;
 
@@ -80,6 +82,33 @@ describe("backpacks", () => {
 
     });
   });
+
+  describe('getBackpacksFromFile', async () => {
+    it('should return two even backpacks in each iteration', async () => {
+      const generator = getBackpacksFromFile(exampleFile);
+
+      const results = (await generator.next()).value
+
+      expect(results.length).toBe(2)
+      expect(results[0].length === results[1].length).toBe(true)
+    })
+
+    it('should return iterations for each file in the file', async () => {
+      const generator = getBackpacksFromFile(exampleFile);
+
+      await generator.next();
+      await generator.next();
+      await generator.next();
+      await generator.next();
+      await generator.next();
+      await generator.next();
+      await generator.next();
+
+      const results = (await generator.next()).value
+
+      expect(results).toBeUndefined()
+    })
+  })
 
   describe("getItemInBothBackpacks", async () => {
     it("should return items in both backpacks", async () => {
