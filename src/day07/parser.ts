3a5dcb0b97806cb46954e5b3e7f5c86dad17ec14
Anthony Porthouse <anthony@porthou.se>
2022-12-08 14:24:29 +0000

    fix: day 7 code smells

--- src/day07/parser.ts
+++ src/day07/parser.ts
@@ -5,53 +5,38 @@ export async function getTreeFromFile(file: string) {
   let stack: Node[] = []
   let currentNode: Node = new Node('/', 'dir');
   for await (const line of getLinesFromFile(file)) {
-    if (line === '') {
-      continue
+    switch (true) {
+      case line === '':
+      case line === '$ ls':
+        break
+
+      case line === '$ cd /':
+        if (stack.length === 0) {
+          break
+        }
+
+        currentNode = stack[0]
+        stack = []
+        break
+
+      case line === '$ cd ..':
+        currentNode = stack.pop() as Node
+        break
+
+      case line.startsWith('$ cd'):
+        stack.push(currentNode)
+        currentNode = currentNode.getNode(line.replace('$ cd ', ''))
+        break
+
+      case line.startsWith('dir '):
+        currentNode.addNode(new Node(line.replace('dir ', ''), 'dir'))
+        break
+
+      default:
+        const [size, name] = line.split(' ', 2);
+        currentNode.addNode(new Node(name, 'file', Number(size)))
     }
-
-    // Change to root
-    if (line === '$ cd /') {
-      if (stack.length === 0) {
-        continue
-      }
-
-      currentNode = stack[0]
-      stack = []
-      continue
-    }
-
-    // Back down a level
-    if (line === '$ cd ..') {
-      currentNode = stack.pop() as Node
-      continue
-    }
-
-    // Change into a given subdirctory
-    if (line.startsWith('$ cd')) {
-      stack.push(currentNode)
-      currentNode = currentNode.getNode(line.replace('$ cd ', ''))
-      continue
-    }
-
-    // This is not needed
-    if (line == '$ ls') {
-      continue
-    }
-
-    // Add a directory
-    if (line.startsWith('dir ')) {
-      currentNode.addNode(new Node(line.replace('dir ', ''), 'dir'))
-      continue
-    }
-
-    // Add a file
-    const [size, name] = line.split(' ', 2);
-    currentNode.addNode(new Node(name, 'file', Number(size)))
   }
 
-  if (stack.length > 0) {
-    return stack[0]
-  }
-
-  return currentNode
+  return stack[0] || currentNode
 }
