210fa42eb7aaf07d45d1fc162b4a8ecda26fcad0
Anthony Porthouse <anthony@porthou.se>
2022-12-10 10:48:15 +0000

    feat: day 10 part 2

--- src/day10/index.test.ts
+++ src/day10/index.test.ts
@@ -1,8 +1,21 @@
 import { describe, expect, it } from "vitest";
-import { exampleFile, runPart1 } from "./index.js";
+import { exampleFile, runPart1, runPart2 } from "./index.js";
 
 describe("runPart1", () => {
   it("should return 13140", async () => {
     expect(await runPart1(exampleFile)).toBe(13140);
+  });
+});
+
+describe("runPart2", () => {
+  it("should return the given output", async () => {
+    expect(await runPart2(exampleFile)).toBe(
+      "##..##..##..##..##..##..##..##..##..##..\n" +
+        "###...###...###...###...###...###...###.\n" +
+        "####....####....####....####....####....\n" +
+        "#####.....#####.....#####.....#####.....\n" +
+        "######......######......######......####\n" +
+        "#######.......#######.......#######.....\n"
+    );
   });
 });
--- src/day10/index.ts
+++ src/day10/index.ts
@@ -24,6 +24,33 @@ export async function runPart1(filename: string) {
   return total;
 }
 
+export async function runPart2(filename: string) {
+  console.time("Day 10 Part 2");
+
+  let index = 0;
+
+  let output = "";
+  for await (const value of runInstructions(filename)) {
+    if (index >= value - 1 && index <= value + 1) {
+      output += "#";
+    } else {
+      output += ".";
+    }
+
+    index += 1;
+
+    if (index === 40) {
+      index = 0;
+      output += "\n";
+    }
+  }
+
+  console.timeEnd("Day 10 Part 2");
+
+  return output;
+}
+
 export async function run() {
   console.log(`Signal Strength: ${await runPart1(inputFile)}`);
+  console.log(`Display: \n${await runPart2(inputFile)}`);
 }
